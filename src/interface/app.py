ec733a22b842ecd9432b04fb92a969673ab41e71
Jerome Blin <blinjrm@gmail.com>
2020-12-13 22:03:28 +0100

    add app homepage

--- src/interface/app.py
+++ src/interface/app.py
@@ -14,9 +14,28 @@ from src.domain.app_utils import newspapers_plot, raw_data_plot, tendency_plot
 def project_description():
 
     st.title("Sentiment analysis of financial headlines")
+    st.text("")
 
     description = """ 
-    ## Some text
+    2020 felt like a rollercoaster 🎢, with the global pandemic causing a sudden economic downturn 📉, which didn't prevent the stock market to rise to new heights ⛰️
+    ---
+    We were curious about how financial newspapers have been dealing with this dichotomy, and decided to conduct an analysis of the sentiment of the headlines they printed throughout the year. 
+
+    By leveraging recent advancements of *transfer learning* in the field of natural language processing, we can take a model pretrained on a large, generic language corpus (think Wikipedia), and fine-tune it on a domain-specific task: in our case classification of financial sentences. 
+
+    For this project, we focused on the United States, being the first economy in the world and home to the largest financial markets, while at the same time being themost affected by the pandemic in terms of total cases and deaths. 
+
+    This website presents in a **dashboard** the results of our analysis, regarding:
+    * The evolution of the sentiment expressed in financial headlines throughout 2020
+    * The relation between that sentiment and:
+        * The stock market
+        * The development of the COVID-19 pandemic
+
+    It also provides you with the opportunity to use the models we fine-tuned on the **headlines** of your choice.  
+
+    Finaly, we compare provide some insights into the **performance** of several language models. 
+
+    *👈 Use the menu on the left to access these screens.*
     """
 
     st.markdown(description)
@@ -24,13 +43,14 @@ def project_description():
 
 def single_sentence():
 
-    st.title("Sentiment analysis on a sentence")
+    st.title("Sentiment analysis on a headline")
     st.text("")
 
     trained_models = list_trained_models()
     trained_models.append("zero-shot-classifier")
 
     model = st.sidebar.radio("Choose a model:", (trained_models))
+    st.sidebar.markdown("---")
 
     sentence = st.text_input(label="Type your sentence here:")
 
@@ -95,6 +115,7 @@ def model_performance():
 
     trained_models = list_trained_models()
     model = st.sidebar.radio("Choose a model:", (trained_models))
+    st.sidebar.markdown("---")
     st.header(f"Showing {model}")
 
 
@@ -107,14 +128,17 @@ def main():
         initial_sidebar_state="auto",
     )
 
+    st.sidebar.text("")
+    st.sidebar.text("")
+
     option = st.sidebar.selectbox(
         "I want to...",
         [
             "",
-            "analyze a single sentence",
             "view the dashboard",
+            "analyze a single headline",
             "compare model performance",
-            "see some ballons",
+            # "see some ballons",
         ],
     )
 
@@ -123,18 +147,18 @@ def main():
 
     if option == "":
         project_description()
-    elif option == "analyze a single sentence":
-        single_sentence()
     elif option == "view the dashboard":
         dashboard()
-    elif option == "compare model performance":
+    elif option == "analyze a single headline":
+        single_sentence()
+    else:  # elif option == "compare model performance":
         model_performance()
-    else:
-        st.balloons()
-        st.sidebar.markdown("Because 2020 wasn't *all* bad.")
-        balloons = st.sidebar.button("Again! 🤩")
-        if balloons:
-            st.balloons()
+    # else:
+    #     st.balloons()
+    #     st.sidebar.markdown("Because 2020 wasn't *all* bad.")
+    #     balloons = st.sidebar.button("Again! 🤩")
+    #     if balloons:
+    #         st.balloons()
 
 
 if __name__ == "__main__":
